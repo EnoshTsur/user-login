1cb2de9257c50317ca649c5d8dcdc6b1b80e0f93
EnoshTs <enosh.tsur@haaretz.co.il>
2020-05-12 19:57:37 +0300

    another

--- src/main/java/com/enosh/userlogin/validate/AddressValidation.java
+++ src/main/java/com/enosh/userlogin/validate/AddressValidation.java
@@ -1,6 +1,17 @@
 package com.enosh.userlogin.validate;
 
+import com.enosh.userlogin.model.Address;
+
 public class AddressValidation {
 
+    private static final String CITY_MESSAGE = "City can not be less than 2 characters";
+    private static final String STREET_MESSAGE = "Street can not be less than 3 characters";
+
+    public static Address validateAddress(Address address) throws IllegalStateException {
+        return Validator.of(address)
+                .validate(Address::getCity, city -> city.length() >= 2, CITY_MESSAGE)
+                .validate(Address::getStreet, city -> city.length() >= 3, STREET_MESSAGE)
+                .get();
+    }
 
 }
