54e489bddedb816b8c47e8ae8d4bfd1202f880e1
EnoshTs <enosh.tsur@haaretz.co.il>
2020-05-12 20:30:44 +0300

    another

--- src/main/java/com/enosh/userlogin/controllers/ControllerUtils.java
+++ src/main/java/com/enosh/userlogin/controllers/ControllerUtils.java
@@ -3,11 +3,20 @@ package com.enosh.userlogin.controllers;
 import com.enosh.userlogin.exceptions.MissingAttributeException;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 public class ControllerUtils {
 
+    public static String missingAttrMessage(String name) {
+        return "Missing attribute: " + name;
+    }
+
     public static String parameterFromRequest(String name, HttpServletRequest request)
             throws MissingAttributeException {
-
+        return Optional.ofNullable(request.getParameter(name))
+                .filter(param -> !param.isEmpty())
+                .orElseThrow(() -> new MissingAttributeException(
+                        missingAttrMessage(name)
+                ));
     }
 }
