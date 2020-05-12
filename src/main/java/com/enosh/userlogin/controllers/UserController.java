dcb30b39669ef4e09ec32626190b35538a7a64e3
EnoshTs <enosh.tsur@haaretz.co.il>
2020-05-12 20:22:45 +0300

    another

--- src/main/java/com/enosh/userlogin/controllers/UserController.java
+++ src/main/java/com/enosh/userlogin/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.enosh.userlogin.controllers;
 
 import com.enosh.userlogin.dao.UserDao;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,15 @@ public class UserController {
 
     public UserController(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    @GetMapping("/signin")
+    public String signin(){
+        return "login/signin";
+    }
+
+    @GetMapping("/signup")
+    public String signup(){
+        return "login/signup";
     }
 }
